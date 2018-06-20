e68cc927ab41670458397a2b2a90b3f251fa6ec4
jintao <jintao.xiao@honeywell.com>
2018-06-20 18:29:03 +0800

    add logic s

---
+++ converters/files/dwgfile-3449c9e5e332f1dbb81505cd739fbf3f.rl
@@ -0,0 +1,5 @@
+C:\HUS+\projects\node-back-demo\converters\files\dwgfile-3449c9e5e332f1dbb81505cd739fbf3f.svg
+[LINKED]
+** empty **
+[UNLINKED]
+** empty **
--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var multer = require('multer');
-var multipart = require('connect-multiparty')
+var multipart = require('connect-multiparty');
+var exec = require('child_process').exec;
+var path = require('path');
+var fs = require('fs');
 
 var app = express();
 var router = express.Router();
 var uploadDwg = require('./control/fileupload');
 var multipartMiddleware = multipart();
+
+var converter = "/converters/ax2019/windows/AX2019.exe";
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -20,13 +25,44 @@ app.get('/', function(req, res) {
     res.send('server started!');
 });
 
-app.post('/upload', multipartMiddleware, function(req, res, next) {
+app.post('/upload', uploadDwg.single('dwgfile'), function(req, res, next) {
     // res.header("Access-Control-Allow-Origin", "*");
     console.log(req.body);
     if(req.file) {
-        res.json({message: '文件上传成功'});
+        // res.json({message: '文件上传成功'});
         console.log(req.file);
         console.log(req.body);
+        let outFileLocation = req.file.path;
+        console.log('url: ', __dirname);
+        console.log('process: ', process.cwd());
+        exec(path.join(__dirname, converter) + ' -i=' + outFileLocation + ' -o='+ path.join(__dirname, 'converters/files/' + req.file.filename.split('.')[0] + '.svg') +' -f="svg" -model -tl="RM$TXT" -rl="RM$"',function(error, stdout, stderr){
+            if(error) {
+                console.error('error: ' + error);
+                // return;
+            }
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+
+            fs.stat(path.join(__dirname, 'converters/files/' + req.file.filename.split('.')[0] + '.svg'), function(err, stats) {
+                if (err) {
+                    return console.error(err);
+                } else {
+                    console.log('stats: ', stats);
+                    if (stats.isFile()) {
+                        fs.readFile(path.join(__dirname, 'converters/files/' + req.file.filename.split('.')[0] + '.svg'), function(err, data){
+                            if(err) {
+                                return console.error(err);
+                            }
+                            console.log('data: ', data);
+                            // console.log('dataString: ', data.toString());
+                            res.send(data.toString());
+                        });
+                    } else {
+                        console.log('file can not get');
+                    }
+                }
+            });
+        })
     } else {
         res.json({'ok': false})
     }
